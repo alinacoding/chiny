526c612f74361a661282873b7fa099526e260f1e
Alina <a.voicu24@gmail.com>
2018-03-14 21:31:42 +0000

    added tesseract

--- server/controllers/imageurl.js
+++ server/controllers/imageurl.js
@@ -1,43 +1,56 @@
 const request = require('request');
 const fs = require('fs');
 var path = require('path');
+const tesseract = require('tesseract.js')
 
-const downloadFile = (imageUrl, filename) => {
+const extractTextFromImage = (filename) => {
+    tesseract.recognize(filename, {lang: "chi_sim"})
+    .then(console.log)
+    .progress(progress => {
+      console.log('progress', progress)
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .then(function (result) {
+      console.log(result.text);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+
+const downloadFile = (imageUrl, filename, translateImage) => {
     request(imageUrl)
       .pipe(fs.createWriteStream(filename))
       .on('close', function() {
-        console.log(filename);
         console.log(`${imageUrl} saved to ${filename}`);
+        translateImage(filename);
+
       })
       .on('error', function(err) {
         console.log(err)
       })
 }
 
-
-const processFile = (imageUrl, filename, downloadFile, sendResponse) => {
-  request.head(imageUrl, filename, function(err, res, body){
+const processUrl = (imageUrl, downloadFile) => {
+  request.head(imageUrl, function(err, res, body){
     console.log('content-type:', res.headers['content-type']);
     const fileExtension = res.headers['content-type'].split('/')[1];
-    filename += fileExtension;
-    downloadFile(imageUrl, filename);
-    sendResponse(filename);
+    const filename = path.resolve(__dirname) + '/image.' + fileExtension;
+    downloadFile(imageUrl, filename, extractTextFromImage);
   });
 };
 
 const handleImageUrl = (req, res) => {
   const { imageUrl } = req.body;
   if (imageUrl) {
-    console.log('received url');
-    let filename = path.resolve(__dirname) + '/image.';
-    processFile(imageUrl, filename, downloadFile, function(filename){
-      res.send(`downloaded ${filename}`);
-    });
+    processUrl(imageUrl, downloadFile);
+    res.send('finished processing the file');
   } else {
     res.status(400).send("Did not receive url");
   }
 }
-
 
 module.exports = {
 	handleImageUrl
